712874d177c89528b425f5d19eb85044c00d810f
Muh Naofal Mufid <valnovc@gmail.com>
2020-10-02 15:34:43 +0700

    Create Teacher Module

---
+++ Modules/Teacher/Providers/TeacherServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Teacher\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class TeacherServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Teacher', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Teacher', 'Config/config.php') => config_path('teacher.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Teacher', 'Config/config.php'), 'teacher'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/teacher');
+
+        $sourcePath = module_path('Teacher', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/teacher';
+        }, \Config::get('view.paths')), [$sourcePath]), 'teacher');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/teacher');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'teacher');
+        } else {
+            $this->loadTranslationsFrom(module_path('Teacher', 'Resources/lang'), 'teacher');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Teacher', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}

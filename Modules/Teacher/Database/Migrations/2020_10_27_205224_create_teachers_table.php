8ce5711764eea1a29522c91b2c1fb6456b6f22cd
Muh Naofal Mufid <valnovc@gmail.com>
2020-10-27 21:57:54 +0700

    Add teacher migrations

---
+++ Modules/Teacher/Database/Migrations/2020_10_27_205224_create_teachers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('password')->nullable();
+            $table->text('address')->nullable();
+            $table->string('avatar')->nullable()->default('https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg');
+            $table->tinyInteger('skill_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
